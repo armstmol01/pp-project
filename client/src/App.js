40b5e210cacdfc5056722c5b4609515c81fac75a
armstmol01 <armstmol01@gmail.com>
2022-08-07 09:17:31 -0700

    rid of footer on ladning pag

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
-import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <>
       <Nav data={{active: 'home', message: ''}}/>
       <Header />
-      <Footer />
     </>
   );
 }
